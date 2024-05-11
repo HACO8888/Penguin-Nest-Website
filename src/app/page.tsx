5e33cc568299bfad655abcfaddc9f6ed443e0d57
HACO8888 <haco.tw@gmail.com>
2024-05-12 02:32:31 +0800

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,42 @@
+"use client"
+
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <></>
+    <div>
+      <img src="/images/Penguin.png" alt="banner" />
+      <div className="w-[65dvw] max-md:w-[100dvw] mt-[2rem] max-md:mt-[1rem] mx-auto">
+        <div className="max-md:mx-[1rem] text-center h-auto w-auto mx-auto rounded-lg py-[1rem]">
+          <p className="text-center md:text-xl">
+            🐧企鵝窩〡競賽社群
+            <br />
+            <br />
+            你想要交友、找人玩遊戲、打比賽嗎?
+            <br />
+            那你來對地方了!! 本群是個競賽社群
+            <br />
+            歡迎揪團一起打GAME、聊聊天等等哦 ~
+            <br />
+            另外我們還會不定時舉辦比賽及抽獎活動哦🎉
+            <br />
+            🐧企鵝窩競賽社群 歡迎您 (👉ﾟヮﾟ)👉
+            <br />
+            <br />
+            🎮本群主要是一個遊戲同樂群🎮
+            <br />
+            ⭐歡迎加入與我們玩遊戲、聊聊天、打比賽哦
+            <br />
+            <br />
+            🔗Discord群連結🔗
+            <br />
+            <Link href={"https://discord.gg/PSefukqYyb"} target="_blenk">https://discord.gg/PSefukqYyb</Link>
+          </p>
+        </div>
+      </div>
+      <div className="text-center justify-center align-middle mt-[2rem] mx-[1rem] mb-[2rem] md:text-lg">
+        <Link className="py-2 px-5 bg-[#355581] rounded-full" href={"https://discord.gg/PSefukqYyb"} target="_blenk">加入 Discord 群組</Link>
+      </div>
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="container fixed left-0 right-0 top-0 z-50 items-center mx-auto max-lg:px-3">
-      <nav className="bg-[#355581] bg-opacity-80 backdrop-blur-xl rounded-xl py-3 px-6 mt-4 lg:py-3 lg:px-10 lg:mt-8 flex items-center justify-between shadow-2xl transition-all">
+    <div className="items-center h-[4rem]">
+      <nav className="bg-[#355581] bg-opacity-80 backdrop-blur-xl flex items-center justify-between shadow-2xl transition-all py-2 px-5">
         <a className="font-bold flex hover:cursor-pointer" href="/">
           <Image src={"/images/logo.png"} height={50} width={50} alt="logo" />
           <p className="text-white font-bold text-xl my-auto ml-3">企鵝窩</p>
