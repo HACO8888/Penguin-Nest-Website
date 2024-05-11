17b8d16e609c65357707edb901e1d1e6b682d200
HACO8888 <haco.tw@gmail.com>
2024-05-12 02:38:38 +0800

    feat: notfound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-white">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-300">The page you are looking for does not exist.</p>
+    <div className="flex flex-col items-center justify-center h-[calc(100dvh-4rem)]">
+      <h1 className="text-[3.5rem] font-bold text-white">404 - 未知的頁面</h1>
+      <p className="text-[1.3rem] text-gray-300 mt-1">您查看的頁面可能已經改變位置或是被刪除了</p>
     </div>
   );
 }
